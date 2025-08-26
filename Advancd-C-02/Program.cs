bfffcdcbcbba987052e08e25b2f14f43a9ddf2eb
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-08-26 06:47:23 +0200

    Q6

--- Advancd-C-02/Program.cs
+++ Advancd-C-02/Program.cs
@@ -1,35 +1,63 @@
-﻿namespace Advancd_C_02
+﻿using System.Collections;
+
+namespace Advancd_C_02
 {
     internal class Program
     {
         #region Q5
         //Given an array, implement a function to remove duplicate elements from an array.
-        public static List<T> RemoveDuplicate<T>(List<T> inputList)
-        {
-            List<T> output = new List<T>();
+        //public static List<T> RemoveDuplicate<T>(List<T> inputList)
+        //{
+        //    List<T> output = new List<T>();
 
-            foreach (var item in inputList)
-            {
-                if (!output.Contains(item))
-                {
-                    output.Add(item);
-                }
-            }
-            return output;
-        }
+        //    foreach (var item in inputList)
+        //    {
+        //        if (!output.Contains(item))
+        //        {
+        //            output.Add(item);
+        //        }
+        //    }
+        //    return output;
+        //}
         #endregion
 
+        #region Q6
+        //Given an array list, implement a function to remove all odd numbers from it.
+        public static void RemoveOdd(ArrayList inputList)
+        {
+            for (int i = inputList.Count - 1; i >= 0; i--)
+            {
+                if (inputList[i] is int number && number % 2 != 0)
+                {
+                    inputList.RemoveAt(i);
+                }
+            }
+        }
+        #endregion
         static void Main(string[] args)
         {
 
 
             #region Q5
-            List<int> inputList = new List<int>() { 1, 2, 3, 4, 4, 5, 5, 6, 7, 8, 8, 9 };
-            List<int> outputList = RemoveDuplicate(inputList);
-            Console.WriteLine("Input List: " + string.Join(", ", inputList));
-            Console.WriteLine("Output List after removing duplicates: " + string.Join(", ", outputList));
+            //List<int> inputList = new List<int>() { 1, 2, 3, 4, 4, 5, 5, 6, 7, 8, 8, 9 };
+            //List<int> outputList = RemoveDuplicate(inputList);
+            //Console.WriteLine("Input List: " + string.Join(", ", inputList));
+            //Console.WriteLine("Output List after removing duplicates: " + string.Join(", ", outputList));
             #endregion
 
+            #region Q6
+            ArrayList inputList = new ArrayList() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach (var item in inputList)
+            {
+                Console.Write(item + " ");
+            }
+            RemoveOdd(inputList);
+            Console.WriteLine("--------------------------------------------------");
+            foreach (var item in inputList)
+            {
+                Console.Write(item + " ");
+            }
+            #endregion
         }
     }
 }
