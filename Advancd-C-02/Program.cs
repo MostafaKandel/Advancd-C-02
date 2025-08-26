6468e91c1489bc351d9c1c2868cd4f258d8eae06
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-08-26 06:29:39 +0200

    Add project files.

---
+++ Advancd-C-02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advancd_C_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
