a902c0a61e0c78731cab2e8555cecce032173b2f
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-08-26 06:41:35 +0200

    Q5

--- Advancd-C-02/Program.cs
+++ Advancd-C-02/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        #region Q5
+        //Given an array, implement a function to remove duplicate elements from an array.
+        public static List<T> RemoveDuplicate<T>(List<T> inputList)
+        {
+            List<T> output = new List<T>();
+
+            foreach (var item in inputList)
+            {
+                if (!output.Contains(item))
+                {
+                    output.Add(item);
+                }
+            }
+            return output;
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Q5
+            List<int> inputList = new List<int>() { 1, 2, 3, 4, 4, 5, 5, 6, 7, 8, 8, 9 };
+            List<int> outputList = RemoveDuplicate(inputList);
+            Console.WriteLine("Input List: " + string.Join(", ", inputList));
+            Console.WriteLine("Output List after removing duplicates: " + string.Join(", ", outputList));
+            #endregion
+
         }
     }
 }
