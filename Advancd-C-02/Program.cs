01ed1cea77c2559a59e604a25c65d493c1a1ee1b
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-08-26 06:53:06 +0200

    Q7

--- Advancd-C-02/Program.cs
+++ Advancd-C-02/Program.cs
@@ -23,16 +23,16 @@ namespace Advancd_C_02
 
         #region Q6
         //Given an array list, implement a function to remove all odd numbers from it.
-        public static void RemoveOdd(ArrayList inputList)
-        {
-            for (int i = inputList.Count - 1; i >= 0; i--)
-            {
-                if (inputList[i] is int number && number % 2 != 0)
-                {
-                    inputList.RemoveAt(i);
-                }
-            }
-        }
+        //public static void RemoveOdd(ArrayList inputList)
+        //{
+        //    for (int i = inputList.Count - 1; i >= 0; i--)
+        //    {
+        //        if (inputList[i] is int number && number % 2 != 0)
+        //        {
+        //            inputList.RemoveAt(i);
+        //        }
+        //    }
+        //}
         #endregion
         static void Main(string[] args)
         {
@@ -46,17 +46,37 @@ namespace Advancd_C_02
             #endregion
 
             #region Q6
-            ArrayList inputList = new ArrayList() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            foreach (var item in inputList)
-            {
-                Console.Write(item + " ");
-            }
-            RemoveOdd(inputList);
-            Console.WriteLine("--------------------------------------------------");
-            foreach (var item in inputList)
-            {
-                Console.Write(item + " ");
-            }
+            //ArrayList inputList = new ArrayList() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //foreach (var item in inputList)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //RemoveOdd(inputList);
+            //Console.WriteLine("--------------------------------------------------");
+            //foreach (var item in inputList)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            #endregion
+
+            #region 7
+            //Implement a queue that can hold different data types. 
+
+            //1- non-generic approach
+            Queue queue = new Queue();
+            queue.Enqueue(1);
+            queue.Enqueue("Apple");
+            queue.Enqueue(5.28);
+
+            foreach (var item in queue) Console.WriteLine(item);
+
+
+            //2- generic approach
+            Queue<object> genericQueue = new Queue<object>();
+            genericQueue.Enqueue(1);
+            genericQueue.Enqueue("Apple");
+            genericQueue.Enqueue(5.28);
+            foreach (var item in genericQueue) Console.WriteLine(item);
             #endregion
         }
     }
